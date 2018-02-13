9214a66eb6ed7428a2bc38a02a0b5b96740e89ac
Bum Kim <bumcrystlbum@gmail.com>
2018-02-13 08:34:09 -0700

    testing c things

---
+++ text_practice.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int str_len(char *char_ptr)
+{
+  int length = 0;
+  int i = 0;
+  while(char_ptr[i] != '\0'){
+    length++;
+    i++;
+  }
+  return length;
+}
+
+void rev_string(char *original)
+{
+  int i = 0;
+  while(original[i] != '\0'){
+    printf("%c", original[i]);
+    i++;
+  }
+}
+
+int main(){
+  char og[] = "aohlcrheusrch";
+  char *og_ptr = og;
+  rev_string(og);
+  int length = str_len(og);
+  printf("%d\n", length);
+
+  return 0;
+}

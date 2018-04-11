fda58dbe051c7daf7747b7b7341cad94db4a138f
Bum Kim <bumcrystlbum@gmail.com>
2018-04-10 22:54:17 -0600

    started pong

---
+++ pygame/pong.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+pygame.init()  # initializes the modules in the pygame library
+size = width, height = 720, 480
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("ball.png")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.draw.lines(screen, (255, 255, 255), True,
+                      ((width/2, 0), (width/2, height)))
+    pygame.display.flip()

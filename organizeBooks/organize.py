44baea1b007e80112079a58cd5b1e1cfdbe44dcb
Bum Kim <bumcrystlbum@gmail.com>
2019-02-01 09:15:10 -0700

    experimenting pulling from open library api

--- organizeBooks/organize.py
+++ organizeBooks/organize.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+import json
+import requests
+
+apiSearch = "http://openlibrary.org/search.json?title="
+
+def searchBook(title):
+    search = apiSearch + title.replace(' ', '+')
+    response = requests.get(search)
+    data = response.json()
+    docs = data["docs"]
+
+    newest = ''
+    for doc in docs:
+        if newest == '':
+            newest = doc
+        elif newest["publish_year"] < doc["publish_year"]:
+            newest = doc
+            
+        
+    print(mostRecent)
+
+
 def enterBook():
     with open('books', 'a') as f:
         title = input("Enter the title of the book: ")
@@ -15,6 +37,7 @@ def main():
     while choice != 'q':
         print("(e)nter a book")
         print("show book (l)ist")
+        print("(s)earch for a title")
         print("(q)uit")
         choice = input()
         if choice == 'q':
@@ -23,6 +46,9 @@ def main():
             enterBook()
         elif choice == 'l':
             readList()
+        elif choice == 's':
+            title = input("Enter a title: ")
+            searchBook(title)
         else:
             print("Not a valid option.")
 
